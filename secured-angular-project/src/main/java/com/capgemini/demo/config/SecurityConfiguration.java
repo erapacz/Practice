6cdbf89848211614d3978339218b72749733bff2
Eric Rapacz <erapacz1@gmail.com>
2018-07-17 12:54:46 -0500

    first commit

--- secured-angular-project/src/main/java/com/capgemini/demo/config/SecurityConfiguration.java
+++ secured-angular-project/src/main/java/com/capgemini/demo/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.capgemini.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,7 +9,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.capgemini.demo.repositories.UsersRepository;
 import com.capgemini.demo.service.CustomUserDetailsService;
@@ -24,31 +26,43 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = passwordEncoder();
 		auth.userDetailsService(userDetailsService)
-		.passwordEncoder(getPasswordEncoder());
+		.passwordEncoder(encoder);
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
-				.antMatchers("**/secured/**").authenticated()
-				.anyRequest().permitAll()
+				.antMatchers("/home", "/login").permitAll()
+				.antMatchers("**/secured/**").hasRole("ADMIN")
+				.anyRequest().fullyAuthenticated()
 				.and()
-				.formLogin().permitAll();
+				.formLogin()
+				.loginProcessingUrl("/login")
+				.permitAll()
+				.and()
+				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/api/v1/players/secured")
+				.and().csrf();
 	}
 	
-	private PasswordEncoder getPasswordEncoder() {
-		return new PasswordEncoder() {
-			@Override 
-			public String encode(CharSequence charSequence) {
-				return charSequence.toString();
-			}
-			
-			@Override
-			public boolean matches(CharSequence charSequence, String s) {
-				return true;
-			}
-		};
+//	private PasswordEncoder getPasswordEncoder() {
+//		return new PasswordEncoder() {
+//			@Override 
+//			public String encode(CharSequence charSequence) {
+//				return charSequence.toString();
+//			}
+//			
+//			@Override
+//			public boolean matches(CharSequence charSequence, String s) {
+//				return true;
+//			}
+//		};
+//	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
